3f9629db95e9e832dafe2e91c9f4015c913a714f
khushi0804 <kg830574@gmail.com>
2025-04-11 11:00:15 +0530

    first commit

--- src/Components/partials/HorizontalCards.jsx
+++ src/Components/partials/HorizontalCards.jsx
@@ -1,16 +1,29 @@
+
 import React from 'react'
-import { Link } from 'react-router-dom'
 
 function HorizontalCards({ data }) {
   return (
-    <div className='w-full p-3'>
-      <div className='w-full flex gap-5 overflow-x-auto p-2'>
-        {data.length > 0 ? data.map((d, i) => (
-          <Link key={i} className="min-w-[15%] h-[35vh] bg-zinc-900 rounded-lg overflow-hidden shadow-lg relative cursor-pointer mb-2">
+    <div className='w-full  p-5' >
+      <div className='mb-5'>
+        <h1 className='text-3xl font-semibold text-zinc-400'>Trending</h1>
+      </div>
+      <div className='w-[100%] flex overflow-y-hidden'>
+        {data.map((d, i) => (
+          <div key={i} className='min-w-[15%] bg-zinc-900 mr-5 mb-5 rounded-lg'>
 
-            <img src={`https://image.tmdb.org/t/p/original${d.backdrop_path || d.poster_path}`} alt="" />
-          </Link>
-        )) : <h1>nothing to show</h1>}
+            <img className='w-full h-[55%] object-cover' src={`https://image.tmdb.org/t/p/original${d.backdrop_path || d.poster_path}`} alt="" />
+
+            <div className=' text-white p-3 h-62'>
+              <h1 className='text-xl font-semibold'>
+                {d.title || d.name || d.original_name || d.original_title}
+              </h1>
+              <p className='text-sm'>
+                {d.overview.slice(0, 49)}...
+                <span className='text-zinc-500'>more</span>
+              </p>
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   )
