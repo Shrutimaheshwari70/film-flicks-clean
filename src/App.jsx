9de30f22f9478473ca36e0f8091dd63bd4718b06
khushi0804 <kg830574@gmail.com>
2025-04-21 10:20:32 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Home from './Components/Home'
 import Trending from './Components/Trending'
+import Popular from './Components/Popular'
+import Movie from './Components/Movie'
+import Tvshows from './Components/Tvshows'
 
 function App() {
   return (
@@ -10,6 +13,10 @@ function App() {
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/trending' element={<Trending />} />
+        <Route path='/popular' element={<Popular />} />
+        <Route path='/movie' element={<Movie />} />
+        <Route path='/tv' element={<Tvshows />} />
+
 
       </Routes>
     </div>
