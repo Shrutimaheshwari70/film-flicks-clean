dd30c803af95476138df0c76511681930b05e633
Shrutimaheshwari70 <Shrutimaheshwari1255@Gmail.com>
2025-07-08 12:05:22 +0530

    fix: corrected tvSlice import extension

--- src/store/actions/tvActions.jsx
+++ src/store/actions/tvActions.jsx
@@ -1,7 +1,7 @@
 import axios from '../../utils/Axios';
-import { loadtv } from '../reducers/tvSlice.jsx';
+import { loadtv,removetv } from '../reducers/tvSlice.jsx';
 
-export { removetv } from '../reducers/tvSlice.jsx';
+// export { removetv } from '../reducers/tvSlice.jsx';
 
 
 
